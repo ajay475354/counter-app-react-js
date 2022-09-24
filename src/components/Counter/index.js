b831bf2a15e2a80c2800847d34ecfb443a6b11fc
ajay475354 <arepalliajaykumar@gmail.com>
2022-09-24 06:55:00 +0000

    counter app

---
+++ src/components/Counter/index.js
@@ -0,0 +1,42 @@
+import {Component} from 'react'
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+
+  onIncriment = () => {
+    this.setState(prevState => {
+      console.log(`previous state of count ${prevState.count}`)
+      return {
+        count: prevState.count + 1,
+      }
+    })
+  }
+  onDecriment = () => {
+    this.setState(prevState => {
+      console.log(`previous state of count ${prevState.count}`)
+      return {
+        count: prevState.count - 1,
+      }
+    })
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">counter</h1>
+        <p className="count">{count}</p>
+        <div>
+          <button className="button" onClick={this.onIncriment}>
+            increase
+          </button>
+          <button className="button" onClick={this.onDecriment}>
+            decrease
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    const role = this.props
+    return <h1>hello ajay kumar {role}</h1>
+  }
+}
+
+export default Welcome
